d2030eb936d60894b294fe6a0f823604566e0124
estiven-lg <estivenlaferre@gmail.com>
2025-06-23 20:59:13 -0600

    first commit

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},  // <-- Este es el cambio clave
+    autoprefixer: {},
+  }
+}
---
+++ src/components/Particle.tsx
@@ -0,0 +1,82 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useCallback, useEffect, useState } from "react";
+import { loadSlim } from "@tsparticles/slim";
+
+function Particle() {
+
+  const [init, setInit] = useState(false);
+
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container: any) => {
+    console.log(container);
+  };
+
+  const params: any = {
+  "background": {
+    "color": { "value": "#000000" }
+  },
+  "particles": {
+    "number": {
+      "value": 800,
+      "density": { "enable": true }
+    },
+    "color": { "value": "#ffffff" },
+    "shape": { "type": "circle" },
+    "size": {
+      "value": { "min": 1, "max": 3 }
+    },
+    "opacity": {
+      "value": { "min": 0.5, "max": 1 }
+    },
+    "zIndex": {
+      "value": 0,          // ← todas al mismo nivel
+      "velocityRate": 10    // ← sin impacto en la velocidad
+    },
+    "move": {
+      "enable": true,
+      "direction": "bottom-right",
+      "speed": 0.9,  
+      // "drift": 1,
+      "straight": false,
+      "outModes": { "default": "out" }
+    },
+    "wobble": {
+      "enable": true,
+      "distance": 10,
+      "speed": { "angle": 10, "move": 10 }
+    }
+  },
+  "fullScreen": { "enable": true }
+}
+
+
+
+  if (!init) {
+    return null; // or a loading spinner
+  }
+
+  return (
+    <Particles
+      id="tsparticles"
+      options={params}
+    />
+
+  );
+}
+
+export default Particle;
